3ecd5fb3b8c8259be39708e5ae8fb202324e6351
Luckny <sluckny@gmail.com>
2024-06-03 19:20:41 -0400

    login page

---
+++ frontend/src/pages/login/index.tsx
@@ -0,0 +1,28 @@
+export default function Login() {
+  return (
+    <div>
+
+      <div>
+        <h1>Sign in</h1>
+      </div>
+      <div>
+        <div>
+          <h3>In order to chat, you must be signed in.</h3>
+        </div>
+
+        <div>
+          <p>Select the service you would like to sign in with</p>
+          <ul>
+            <li><a href="/auth/login/facebook">Facebook</a></li>
+            <li>
+              <a href="/auth/login/github">Github</a>
+            </li>
+            <li>
+              <a href="/auth/login/google">Google</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div >
+  )
+}
