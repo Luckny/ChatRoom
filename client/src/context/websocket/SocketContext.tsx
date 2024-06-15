d98275899cee450347dcf2cddf1772cd217f7131
Luckny <luckny.simelus@gmail.com>
2024-06-15 19:30:38 -0400

    fix socket link

--- client/src/context/websocket/SocketContext.tsx
+++ client/src/context/websocket/SocketContext.tsx
@@ -18,7 +18,7 @@ export function SocketProvider({ children }: ChildrenType) {
   const [webSocket, setWebsocket] = useState<WebSocket | null>(null);
   const [messages, setMessages] = useState<string[]>([]);
   useEffect(() => {
-    const ws = new WebSocket(`ws://localhost:8080/room?token=mytoken`);
+    const ws = new WebSocket(`ws://localhost:8080/linkup`);
 
     ws.onopen = () => {
       console.log('Web Socket connection opened.');
