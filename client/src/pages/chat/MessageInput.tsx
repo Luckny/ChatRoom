f078f13b42fac576cff777d91b8fa028b4a53136
Luckny <luckny.simelus@gmail.com>
2024-06-18 10:17:49 -0400

    center message input element

--- client/src/pages/chat/MessageInput.tsx
+++ client/src/pages/chat/MessageInput.tsx
@@ -9,19 +9,30 @@ type InputProps = {
 
 export default function MessageInput({ onSubmit, addMessage }: InputProps) {
   return (
-    <Grid container style={{ padding: '20px' }}>
-      <Grid component="form" onSubmit={onSubmit}>
-        <TextField
-          id="outlined-basic-email"
-          label="Type Something"
-          fullWidth
-          onChange={(event: ChangeEvent<HTMLInputElement>) =>
-            addMessage(event.target.value)
-          }
-        />
-        <Fab color="primary" size="small" aria-label="add" type="submit">
-          <SendIcon />
-        </Fab>
+    <Grid container style={{ padding: '20px' }} alignItems="center">
+      <Grid item xs={12}>
+        <form
+          onSubmit={onSubmit}
+          style={{ display: 'flex', alignItems: 'center' }}
+        >
+          <TextField
+            id="outlined-basic-email"
+            label="Type Something"
+            fullWidth
+            onChange={(event: ChangeEvent<HTMLInputElement>) =>
+              addMessage(event.target.value)
+            }
+          />
+          <Fab
+            color="primary"
+            size="small"
+            aria-label="add"
+            type="submit"
+            style={{ marginLeft: '10px' }}
+          >
+            <SendIcon />
+          </Fab>
+        </form>
       </Grid>
     </Grid>
   );
